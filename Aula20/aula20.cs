e3debfaa9c392bdaf0da3ca86c00878f602732c3
MARCO AURÉIO DA SILVA <68288453+marco82silva@users.noreply.github.com>
2020-07-23 10:41:51 -0300

    commit

---
+++ Aula20/aula20.cs
@@ -0,0 +1,17 @@
+using System;
+class aula20{
+
+    static void Main(){
+    
+    int[] num=new int[10];
+    
+    int i=0;
+    while(i<num.Length){
+        num[i]=0;
+        Console.WriteLine(num[i]);
+        i++;
+    }
+    Console.WriteLine("Fim do loop");
+        }
+   // }
+}
